4528251470645ea5e3584d3339a05cab30a79f6b
Saurav Adhikari <sauravads123@gmail.com>
2021-03-29 18:22:44 +0545

    feat : streaming

--- public/script.js
+++ public/script.js
@@ -1,2 +1,31 @@
 const socket = io("/")
-socket.emit("join-room",ROOM_ID,1)
+const videoGrid = document.querySelector("#video-grid");
+const myVideo = document.createElement("video");
+myVideo.muted=true;
+navigator.mediaDevices.getUserMedia({
+    video:true,
+    audio:true
+}).then( stream=>{
+    addVideoStream(myVideo,stream)
+    socket.on("user-connected",userID=>{
+        connectToNewUser(userID,stream)
+    })
+})
+
+socket.emit("join-room",ROOM_ID,USER_ID)
+socket.on("user-connected",userID=>{
+    console.log(`USER CONNECTED ${userID}`)
+
+});
+
+function addVideoStream(video,stream){
+    video.srcObject=stream;
+    video.addEventListener("loadedmetadata",()=>{
+        video.play();
+    })
+    videoGrid.appendChild(video)
+}
+function connectToNewUser(video,stream){
+    addVideoStream(video,stream)
+  
+}
--- server.js
+++ server.js
@@ -3,21 +3,27 @@ const app = express();
 const server = require("http").Server(app)
 const io = require("socket.io")(server);
 const {v4 : uuidV4} = require("uuid");
+const cors=require("cors")
 app.set("view engine","ejs")
 app.use(express.static("public"))
+app.use(cors());
 
 app.get("/",(req,res)=>{
     res.redirect(`/${uuidV4()}`)
 })
 
+
 io.on("connection",socket=>{
     socket.on("join-room",(roomID,userID)=>{
         console.log(roomID,userID);
+        socket.join(roomID)
+        socket.broadcast.to(roomID).emit("user-connected",userID)
     })
 })
 app.get("/:room",(req,res)=>{
+    const userID=uuidV4();
     const roomID = req.params?.room;
-    res.render('room',{roomID})
+    res.render('room',{roomID,userID})
 })
 
 const PORT = process.env.PORT || 5000
